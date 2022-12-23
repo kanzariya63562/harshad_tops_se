f511935da736043bcae4672374fb8b342a1ba43e
kanzariya63562 <harshadbhai27840@gmail.com>
2022-12-23 10:56:53 +0530

    Update test1.c

--- c programming/classwork/test1.c
+++ c programming/classwork/test1.c
@@ -2,9 +2,17 @@
 
 void main()
 {
-	printf("Fname :Harshad");
-	printf("\nLname :Kanzariya");
-	printf("\nAddress :Chuda");
-	printf("\nD.O.B :13 sep 2001");
+	int no = 29;
+	char ch = 'u';
+	float per = 58.78;
+	double stock = 7899987789.4545454545;
+	char name[10]="tops tec";
+	char mobile[12] = "9594789787";
 	
-} 
+	printf("%d\n",no);
+	printf("%c\n",ch);
+	printf("%.2f",per);
+	printf("\n%1f",stock);
+	printf("%s\n,name");
+	printf("%s",mobile);
+}
